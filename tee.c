0f919d8144bc7c99eeca6de31ed7d18d180f438e
mxwilson <matthewxwilson@gmail.com>
2016-10-20 18:28:50 -0400

    tee

--- tee.c
+++ tee.c
@@ -1,75 +1,93 @@
 #include<stdio.h>
-#include<getopt.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include<sys/stat.h>
 #include<string.h>
+#include<getopt.h>
+#include<unistd.h>
 #include<errno.h>
+#include<stdlib.h>
 #include<signal.h>
+#include<fcntl.h>
 
-// TEE clone 0.1. MWilson, Oct 2016.
+// TEE clone 0.2. MWilson, Oct 2016.
 // based on Tee from GNU Coreutils. 
 // License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
 
-void sigproc();
-int leftover=0;
-int a=0;
-int ii=0;
-FILE* fp;
+// order of ops: main to filechecker. get actual files and go to charcatcher. after every line entered, go to filewriter.
 
-thefunc(int argcc, char*argvv[]) {
-int x;
+int a = 0;
+int ii = 0;
+static int errcode = 0;
+static int repeatchars = 0;
 
-// file modes: append or overwrite
+filechecker(int argc, char*argv[]) {
+	struct stat sb;
+	int x;
+	char* filestowrite[999];
+	int numfiles = 0;
 
-char* themode;
-if (a == 1) {
-	themode = "a";
-}
-else {
-	themode = "w";
-}
-
-char* thefiles[50];
-int filecnt = 0;
-
-// loop thru args. keep files that do not exist/or have no permissions problems
-
-for (x = 0; x < leftover; x++) {
-	if ( (access(argvv[x], F_OK) == -1) || (access(argvv[x], R_OK|W_OK) != -1) ) {
-		thefiles[filecnt] = malloc(strlen(argvv[x]) + 1);
-		strcpy(thefiles[filecnt], argvv[x]);
-		filecnt++;	
+	for (x = 0; x < argc; x++) {
+		// catch doubling of putchar when using '-' as argv
+		if (strcmp(argv[x], "-") == 0) {
+			repeatchars = 1;
+			continue;
+		}
+		if ((stat(argv[x], &sb) == 0) && (S_ISDIR(sb.st_mode))) { 
+			printf("error: %s is a directory\n", argv[x]);
+			errcode = 1;
+			continue;
+		}
+		// disregard files without r/w. non-exist files may pass
+		if ((access(argv[x], R_OK|W_OK) == -1) && (access(argv[x], F_OK) != -1)) {
+	        	fprintf(stderr, "%s: %s\n", argv[x], strerror(errno));
+			errcode = 1;
+			continue;
+		}					
+		filestowrite[numfiles] = malloc(strlen(argv[x]) + 1);
+		strcpy(filestowrite[numfiles], argv[x]);
+		numfiles++;
 	}
-	else {
-		fprintf(stderr, "%s: %s\n", argvv[x], strerror(errno));
-	}
-
-}
-
-// collect and display the stdin lines
-char line[999];
-int inputlines=0;
-char* mylines[99];
-
-while (fgets(line, sizeof line, stdin) != NULL) {
-	printf("%s", line);
-	mylines[inputlines] = malloc(strlen(line) + 1);
-	strcpy(mylines[inputlines], line);
-	inputlines++;
-}
-
-// then write lines to files
-
-int y;
-for (x = 0; x < filecnt; x++) {
-	fp = fopen(thefiles[x], themode);
 	
-	for (y = 0; y < inputlines; y++) {
-		fprintf(fp, "%s", mylines[y]);
+	int fd;	
+
+	for (x = 0; x < numfiles; x++) {
+		// if not appending truncate the files first
+		if (a != 1) {
+			fd = open(filestowrite[x], O_TRUNC);
+			close(fd);
+		}		
+		
 	}
-	fclose(fp);
+	// now visit getchar/putchar routine
+	charcatcher(x, numfiles, filestowrite);
+	return(0);
 }
-return(0);
+
+charcatcher(int x, int numfiles, char*filestowrite[999]) {
+	char line[999];
+        int linecnt = 0;
+        // print the stdin and write to file immediately
+        while (fgets(line, sizeof line, stdin) != NULL) {
+		printf("%s", line);
+                
+		for (x = 0; x < numfiles; x++) { 
+                	filewriter(x, numfiles, filestowrite, line);
+                }
+		//double print if "-" on command line
+		if (repeatchars == 1) {                	
+			printf("%s", line);
+               	}
+        }
+	return(0);
+}
+
+filewriter(int x, int numfiles, char*filestowrite[999], char line[999]) {
+	FILE* fp;
+	for (x = 0; x < numfiles; x++) {
+		fp = fopen(filestowrite[x], "a");
+		fprintf(fp, "%s", line);
+		fclose(fp);
+	}
+	return(0);
 }
 
 static int flag_help;
@@ -83,8 +101,13 @@ static struct option const long_options[] =
 };
 
 usage() {
-printf("usage: tee [file1] [file2] -a (append) -i (ignore interrupt)\n");
-exit(EXIT_FAILURE);
+	printf("usage: tee [file1] [file2] -a (append) -i (ignore interrupt)\n");
+	if (flag_help) {
+		exit(EXIT_SUCCESS);
+	}
+	else {
+		exit(EXIT_FAILURE);
+	}
 }
 
 void sigproc() {
@@ -92,48 +115,50 @@ void sigproc() {
 }
 
 int main (int argc, char* argv[]) {
-int optc;
-int index;
+	int optc;
+	int index;
 
-while ((optc = getopt_long (argc, argv, "ai", long_options, (int *) 0)) != EOF) {
-	switch (optc) {
-		case 0:
-		    break; 
-	   	case 'a':
-	            a=1;
-		    break;
-	   	case 'i':
-	            ii=1;
-		    break;
-		case '?':
-		    usage();
-		    break;
-	   	default:	
-		    usage();
-		    break;
+	while ((optc = getopt_long (argc, argv, "ai", long_options, (int *) 0)) != EOF) {
+		switch (optc) {
+			case 0:
+		    	    break; 
+	   		case 'a':
+	            	    a = 1;
+		    	    break;
+	   		case 'i':
+	            	    ii = 1;
+		    	    break;
+			case '?':
+		    	    usage();
+		    	    break;
+	   		default:	
+		    	    usage();
+		    	    break;
+		}
 	}
-}
+	
+	if (flag_help) {
+		usage();
+	}
+	// ignore signal ^C
+	if (ii == 1) {
+		signal(SIGINT, sigproc);
+	}
 
-if (flag_help) {
-	usage();
-}
-
-if (argc > 1) {
 	for (index = optind; index < argc; index++) {
 		;
 	}
 
 	argc -= optind;
 	argv += optind;
-	leftover = index-optind;
-}
 
-// ignore signal ^C
-if (ii == 1) {
-	signal(SIGINT, sigproc);
-}
-
-// call primary function
-thefunc(argc, argv);
-exit(EXIT_SUCCESS);
+	if (filechecker(argc, argv) != 0) {
+		exit(EXIT_FAILURE);
+	}
+	else if (errcode != 0) {
+		exit(EXIT_FAILURE);
+	}
+	else {
+		exit(EXIT_SUCCESS);
+	}
 }
