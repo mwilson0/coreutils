6682b2780f3d99943dfc7b17faf390681455aeaa
mxwilson <matthewxwilson@gmail.com>
2016-12-19 18:49:53 -0500

    cp

--- cp.c
+++ cp.c
@@ -9,71 +9,98 @@
 #include<time.h>
 #define BUF_SIZE 9999
 
-// cp clone 0.1.1. matthew wilson. july 2015. 
+// cp clone 0.1.2. matthew wilson. 2015, 2016
 // based on cp from GNU Coreutils 8.2. No warranty.
 // License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
 
-int leftover=0;
-int i=0, v=0, u=0;
+int i = 0, v = 0, u = 0;
 
-thefunc(int argcc, char*argvv[]) {
-int x;
-struct stat fileStat;
-time_t raw[2];
-int c;
-ssize_t file_in, file_out;
-char buffer[BUF_SIZE];
-int sourcefile, destfile;
+int thefunc(int argc, char*argv[]) {
+	int x;
+	struct stat fileStat;
+	time_t raw[2];
+	int c;
+	ssize_t file_in, file_out;
+	char buffer[BUF_SIZE];
+	int sourcefile, destfile;
 
-// test source file
-sourcefile=open(argvv[0], O_RDONLY);
+	// test source file
+	sourcefile=open(argv[0], O_RDONLY);
 
-if (sourcefile == -1) {
-	printf("cannot stat '%s': No such file or directory\n", argvv[0]);
-	exit(1);
-}
+	if (sourcefile == -1) {
+		printf("cannot stat '%s': No such file or directory\n", argv[0]);
+		return(1);
+	}
 
-// check if dest file exists
-if (access(argvv[1], F_OK|R_OK|W_OK) != -1) {
-	if (i == 1 & u != 1) { 
+	// check if dest file exists
+	if (access(argv[1], F_OK|R_OK|W_OK) != -1) {
 		// interactive mode
-		printf("overwrite '%s'? ", argvv[1]);
-		scanf("%s", &c);	
-		if (c!='y') {
-			exit(0);
+		if (i == 1 & u != 1) { 
+			printf("overwrite '%s'? ", argv[1]);
+			scanf("%s", &c);	
+			if (c != 'y') {
+				return(0);
+			}
+		}
+
+		// update mode. compare times of source and dest files
+		if (u == 1) {
+			for (x = 0; x < argc; x++) {
+				stat(argv[x], &fileStat);
+				raw[x] = fileStat.st_mtime;
+			}
+			if (raw[0] < raw[1]) {
+				printf("no overwrite (^.^)\n");
+				return(0);
+			}
 		}
 	}
 
-	// update mode. compare times of source and dest files
-	if (u == 1) {
-		for (x=0; x<leftover; x++) {
-		stat(argvv[x], &fileStat);
-		raw[x]=fileStat.st_mtime;
-		}
-		if (raw[0] < raw[1]) {
-			printf("no overwrite (^.^)\n");
-			exit(0);
-		}
+	// first append if using ../
+	if ((strncmp(argv[1], "../", 3) == 0) && strlen(argv[1]) == 3) {
+		strcat(argv[1], argv[0]);
 	}
-}
 
-destfile=creat(argvv[1], 0666);
+	// now check if src and dest are same
 
-while ((file_in=read(sourcefile, &buffer, BUF_SIZE)) >0 ) {
-	file_out=write(destfile, &buffer, (ssize_t) file_in);
+	if (strcmp(argv[0], argv[1]) == 0) {
+		printf("error: same file\n");
+		return(1);
+	}
+
+	if (strstr(argv[1], "./")) {
+		if (strcmp(argv[0], argv[1]+2) == 0) {
+			printf("error: same file\n");
+			return(1);
+		}
+	} 
+
+	if (strstr(argv[0], "./")) {
+		if (strcmp(argv[1], argv[0]+2) == 0) {
+			printf("error: same file\n");
+			return(1);
+		}
+	} 
+
+	// otherwise do some copying
+
+	destfile=creat(argv[1], 0666);
+
+	while ((file_in = read(sourcefile, &buffer, BUF_SIZE)) > 0) {
+		file_out = write(destfile, &buffer, (ssize_t) file_in);
 		if (file_out != file_in) {
 			printf("write error\n");
-			exit(1);
+			return(1);
 		}
-}
+	}
 
-// verbose mode
-if (v == 1) {
-	printf("%s -> %s\n", argvv[0], argvv[1]);
-}
+	// verbose mode
+	if (v == 1) {
+		printf("%s -> %s\n", argv[0], argv[1]);
+	}
 
-close(sourcefile); close(destfile);
-exit(0);
+	close(sourcefile); close(destfile);
+	return(0);
 }
 
 static int flag_help;
@@ -87,66 +114,70 @@ static struct option const long_options[] =
    	{0, 0, 0, 0}			
 };
 
-usage() {
-printf("usage: cp [source file] [dest file] [-ivu]\n");
-exit(1);
-}
-
-main (int argc, char* argv[]) {
-int optc;
-int index;
-
-while ((optc = getopt_long (argc, argv, "ivu", long_options, (int *) 0)) !=EOF) {
-	switch (optc) {
-	    case 0:
-		break; 
-	    case 'i':
-	        i=1;
-		break;
-	    case 'v':
-	        v=1;
-		break;
-	    case 'u':
-	        u=1;
-		break;
-	    case '?':
-		usage();
-		break;
-	    default:	
-		usage();
-		break;
+int usage() {
+	printf("usage: cp [source file] [dest file] [-ivu]\n");
+	if (flag_help) {
+		exit(EXIT_SUCCESS);
+	}
+	else {
+		exit(EXIT_FAILURE);
 	}
 }
 
-if (flag_help) {
-	usage();
-}
+int main (int argc, char* argv[]) {
+	int optc;
+	int index;
 
-if (argc == 1) {
-	printf("cp: missing file operand\n");
-	usage();
-}
+	while ((optc = getopt_long (argc, argv, "ivu", long_options, (int *) 0)) != EOF) {
+		switch (optc) {
+	    	    case 0:
+		    	break; 
+	    	    case 'i':
+	              	i = 1;
+		      	break;
+	    	    case 'v':
+	        	v = 1;
+			break;
+	    	    case 'u':
+	            	u = 1;
+			break;
+	    	    case '?':
+		        usage();
+			break;
+	            default:	
+			usage();
+			break;
+		}
+	}
 
-else if (argc > 1) {
-	for (index=optind; index<argc; index++) {
-		;
+	if (flag_help) {
+		usage();
 	}
 
 	argc -= optind;
 	argv += optind;
-	leftover=index-optind;
-
-	if (leftover == 1) {
+	
+	for (index = optind; index < argc; index++) {
+		;
+	}
+	
+	if (argc < 2) {
+		printf("cp: missing file operand\n");
+		usage();
+	}
+	else if (argc == 1) {
 		printf("cp: missing destination file operand after '%s'\n", argv[0]);
 		usage();
 	}
-	if (leftover == 2) {
-		thefunc(argc, argv);
+	else if (argc == 2) {
+		if (thefunc(argc, argv) == 0) {
+			exit(EXIT_SUCCESS);
+		}
+		else {
+			exit(EXIT_FAILURE);
+		}
 	}
-	if (leftover > 2) {
+	else {
 		usage();
 	}
-
-}
-
 }
